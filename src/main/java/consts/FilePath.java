a13ae8cec021e7c993d7f49f50d024db1348e17c
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-07 17:08:21 +0900

    feat(FilePath): 파일 경로 상수 저장 클래스 선언

---
+++ src/main/java/consts/FilePath.java
@@ -0,0 +1,11 @@
+package consts;
+
+public class FilePath {
+    static public final String MD_PRODUCTS = "src/main/resources/products.md";
+    static public final String MD_PROMOTIONS = "src/main/resources/promotions.md";
+
+
+    private FilePath() {
+        // 인스턴스화 방지를 위해서 사용
+    }
+}
