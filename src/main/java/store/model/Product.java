ca3c0cc2771e24cf73eae86768bf8eeee76c2b94
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-07 14:46:27 +0900

    feat: Product의 필드값 메서드와toString() 추가
    - 각각 필드값 수정 기능
    - 가각 필드값 출력 기능
    - 객체 필드값을 하나의 문자열로 출력 기능

--- src/main/java/store/model/Product.java
+++ src/main/java/store/model/Product.java
@@ -13,4 +13,43 @@ public class Product {
         this.promotion = promotion;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(String promotion) {
+        this.promotion = promotion;
+    }
+
+    public String toString() {
+        String result = "Name: " + name + ", Price: " + price + ", Quantity: " + quantity + ", Promotion: " + promotion;
+
+        return result;
+
+    }
+
 }
