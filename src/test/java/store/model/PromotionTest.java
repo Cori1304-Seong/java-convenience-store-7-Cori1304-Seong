20c744f4b48a7385b6058db394e57eff4dac2410
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-07 16:28:38 +0900

    test: Promotion 테스트 함수 작성
    객체 필드 = (name, buyCount, getCount, startDate, endDate)
    - 객체 필드 확인 테스트
    - 객체 필드 수정 테스트
    - 객체 필드 정보 문자열 테스트

--- src/test/java/store/model/PromotionTest.java
+++ src/test/java/store/model/PromotionTest.java
@@ -1,16 +1,83 @@
 package store.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Date;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class PromotionTest {
+    Date START_DATE = new Date(2024, 11, 1);
+    Date END_DATE = new Date(2024, 12, 1);
 
     private Promotion promotion;
 
     @BeforeEach
     public void setUp() {
-        Date _startData = new Date(2024, 11, 1);
-        Date _endData = new Date(2024, 12, 1);
-        promotion = new Promotion("반짝할인", 3, 1, _startData, _endData);
+        promotion = new Promotion("반짝할인", 3, 1, START_DATE, END_DATE);
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("반짝할인", promotion.getName());
+    }
+
+    @Test
+    public void testSetName() {
+        promotion.setName("setName");
+        assertEquals("setName", promotion.getName());
+    }
+
+    @Test
+    public void testGetBuy() {
+        assertEquals(3, promotion.getBuyCount());
+    }
+
+    @Test
+    public void testSetBuy() {
+        promotion.setBuyCount(6);
+        assertEquals(6, promotion.getBuyCount());
+    }
+
+    @Test
+    public void testGetGetCount() {
+        assertEquals(1, promotion.getGetCount());
+    }
+
+    @Test
+    public void testSetGetCount() {
+        promotion.setGetCount(5);
+        assertEquals(5, promotion.getGetCount());
+    }
+
+    @Test
+    public void testGetStartDate() {
+        assertEquals(START_DATE, promotion.getStartDate());
+    }
+
+    @Test
+    public void testSetStartDate() {
+        Date date = new Date(2024, 10, 10);
+        promotion.setStartDate(date);
+        assertEquals(date, promotion.getStartDate());
+    }
+
+    @Test
+    public void testGetEndDate() {
+        assertEquals(END_DATE, promotion.getEndDate());
+    }
+
+    @Test
+    public void testSetEndDate() {
+        Date date = new Date(2024, 12, 10);
+        promotion.setStartDate(date);
+        assertEquals(date, promotion.getStartDate());
+    }
+
+    @Test
+    public void testToString() {
+        String expected = "Name: " + promotion.getName() + ", Buy: " + promotion.getBuyCount() + ", Get: "
+                + promotion.getGetCount() + ", start_date: " + START_DATE.toString() + ", end_date: "
+                + END_DATE.toString();
+        assertEquals(expected, promotion.toString());
     }
 }
