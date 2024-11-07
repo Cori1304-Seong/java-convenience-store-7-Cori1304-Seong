9dae4c4b7be7894fd0453e9bfb9ce37989d04477
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-08 03:15:50 +0900

    test: 프로모션 진행 여부로 나뉨 Product 데이터 테스트 추가
    - testGetProductGroup(): 일반 Product에 프로모션 진행 중인지 확인
    - testGetPromotionProductGroup() :  프로모션 Product에 프로모션 없는 지 확인

--- src/test/java/store/controller/ProductControllerTest.java
+++ src/test/java/store/controller/ProductControllerTest.java
@@ -1,9 +1,15 @@
 package store.controller;
 
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import store.model.Product;
 
 class ProductControllerTest {
     private ProductController controller;
+
+    private String PROMOTION_NULL_MESSAGE = "promotion 필드가 null입니다.";
+    private String PROMOTION_NOT_NULL_MESSAGE = "promotion 필드에 값이 있습니다..";
 
     @BeforeEach
     public void setUp() {
@@ -11,4 +17,38 @@ class ProductControllerTest {
         controller.init();
     }
 
+    @Test
+    public void testGetProductGroup() {
+        Map<String, Product> _productGroup = controller.getProductGroup();
+
+        for (Map.Entry<String, Product> entry : _productGroup.entrySet()) {
+            Product product = entry.getValue();
+
+            if (isPromotionNull(product) == false) {
+                throw new RuntimeException(PROMOTION_NOT_NULL_MESSAGE);
+            }
+        }
+    }
+
+    @Test
+    public void testGetPromotionProductGroup() {
+        Map<String, Product> _productGroup = controller.getPromotionProductGroup();
+
+        for (Map.Entry<String, Product> entry : _productGroup.entrySet()) {
+            Product product = entry.getValue();
+
+            if (isPromotionNull(product)) {
+                throw new RuntimeException(PROMOTION_NULL_MESSAGE);
+            }
+        }
+    }
+
+    private Boolean isPromotionNull(Product product) {
+        String promotion = product.getPromotion();
+        if (promotion.equals("null") || promotion.isBlank() || promotion.isEmpty()) {
+
+            return true;
+        }
+        return false;
+    }
 }
