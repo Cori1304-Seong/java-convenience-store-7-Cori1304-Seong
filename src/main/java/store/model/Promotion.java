4d2606a26c99065e55be7d4c87676cc8132c6fcd
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-07 15:29:30 +0900

    feat: Promotion 필드 과련 메서드와 toString() 추가
    - 각각 필드값 수정 기능
    - 가각 필드값 출력 기능
    - 객체의 필드값을 하나의 문자열로 출력 기능

--- src/main/java/store/model/Promotion.java
+++ src/main/java/store/model/Promotion.java
@@ -2,6 +2,7 @@ package store.model;
 
 import java.util.Date;
 
+// TODO 필드 이름 promotions.md와 다르게 해도 되는지 고민
 public class Promotion {
     String name;
     int buy;
@@ -16,4 +17,46 @@ public class Promotion {
         this.startDate = startDate;
         this.endDate = endDate;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public void setGet(int price) {
+        this.get = get;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEnd(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    // TODO 필드 이름 변경시 같이 변경
+    public String toString() {
+        String result = "Name: " + name + ", Buy: " + buy + ", Get: " + get + ", start_date: " + startDate + "end_date:"
+                + endDate;
+
+        return result;
+
+    }
+
 }
