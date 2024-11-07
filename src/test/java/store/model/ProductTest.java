d2267692884225528d8d801ca622ca06321a1c6c
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-07 14:55:50 +0900

    feat: Product 테스트 함수 작성
    객체 필드 = (name, price, quantity, promotion)
    - 객체 필드  확인 테스트
    - 객체 필드 수정 테스트
    - 객체 필드 정보 문자열 테스트
    
    Product 모델이 가지고 있는 메서드 기능 테스 함수를 작성했습니다

--- src/test/java/store/model/ProductTest.java
+++ src/test/java/store/model/ProductTest.java
@@ -1,6 +1,8 @@
 package store.model;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
     private Product product;
@@ -8,5 +10,55 @@ class ProductTest {
     @BeforeEach
     public void setUp() {
         product = new Product("콜라", 1000, 10, "탄산2+1");
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("콜라", product.getName());
+    }
+
+    @Test
+    public void testSetName() {
+        product.setName("사이다");
+        assertEquals("사이다", product.getName());
+    }
+
+    @Test
+    public void testGetPrice() {
+        assertEquals(1000, product.getPrice());
+    }
+
+    @Test
+    public void testSetPrice() {
+        product.setPrice(1200);
+        assertEquals(1200, product.getPrice());
+    }
+
+    @Test
+    public void testGetQuantity() {
+        assertEquals(10, product.getQuantity());
+    }
+
+    @Test
+    public void testSetQuantity() {
+        product.setQuantity(15);
+        assertEquals(15, product.getQuantity());
+    }
+
+    @Test
+    public void testGetPromotion() {
+        assertEquals("탄산2+1", product.getPromotion());
+    }
+
+    @Test
+    public void testSetPromotion() {
+        product.setPromotion("null");
+        assertEquals("null", product.getPromotion());
+    }
+
+    @Test
+    public void testToString() {
+        String expected = "Name: 콜라, Price: 1000, Quantity: 10, Promotion: 탄산2+1";
+        assertEquals(expected, product.toString());
     }
 }
