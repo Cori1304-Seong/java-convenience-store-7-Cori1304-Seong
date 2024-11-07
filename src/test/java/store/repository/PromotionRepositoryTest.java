c3da832847b25f34f4a865dbd22cdba6e4ddd316
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-07 19:16:59 +0900

    test(PromotionRepositoryTest): 테스트 객체 추가

---
+++ src/test/java/store/repository/PromotionRepositoryTest.java
@@ -0,0 +1,14 @@
+package store.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class PromotionRepositoryTest {
+
+    private PromotionRepository repository;
+
+    @BeforeEach
+    public void setUp() {
+        repository = new PromotionRepository();
+    }
+
+}
