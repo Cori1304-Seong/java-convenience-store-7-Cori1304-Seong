9637059e25a7e9a66fbd5fca8bd2d05cdf739404
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-07 14:34:46 +0900

    feat(BaseRepository): 모든 repository에서 상속할 class 추가
    - filePath, filePath 변경 가능
    - loadDataFromMarkdown(): .md 파일을 로드할때 사용됨
    
    .md의 기록된 데이터를 가져오는 것은 Product와 Promotion 전부 동일한 기능이기 떄문에 상속을 받으면 직관적이게 구현 가능할거라고 판단했고 나중에 새로운 .md를 로드할때도 사용할 수 있기에 만들었습니다

---
+++ src/main/java/store/repository/BaseRepository.java
@@ -0,0 +1,48 @@
+package store.repository;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BaseRepository<T> {
+    private String filePath;
+    private String regex = ",";
+    private String line;
+    protected List<T> modelDataEntries = new ArrayList<>();
+
+    public BaseRepository(String filePath, String regex) {
+        this.filePath = filePath;
+        this.regex = regex;
+    }
+
+    public BaseRepository(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public List<T> loadDataFromMarkdown(String startsWith) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            while ((line = br.readLine()) != null) {
+                addModelData(line, startsWith);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage()); //TODO View로 따로 분해 예정
+        }
+        return modelDataEntries;
+    }
+
+    private void addModelData(String _line, String startsWith) {
+        if (_line.isEmpty() || _line.startsWith(startsWith)) {
+            return;
+        }
+        String[] data = _line.split(regex);
+        T instance = parseData(data);
+        if (instance != null) {
+            modelDataEntries.add(instance);
+        }
+    }
+i
+    /* Model마다 colum이 다르기 떄문에 파싱 기능은 저의 필요 */
+    protected abstract T parseData(String[] data);
+}
