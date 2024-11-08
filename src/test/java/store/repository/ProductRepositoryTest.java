a8de600f9d5f449b35e43dbd0819c76b70be3af4
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-09 02:32:52 +0900

    feat: promotion "null"이 아닌 ""으로 변경
    - ""로 변경하여 더 직관적이게 파악가능
    - 테스트 완료

--- src/test/java/store/repository/ProductRepositoryTest.java
+++ src/test/java/store/repository/ProductRepositoryTest.java
@@ -10,21 +10,21 @@ import store.model.Product;
 class ProductRepositoryTest {
     private final List<String> productData = List.of(
             "Name: 콜라, Price: 1000, Quantity: 10, Promotion: 탄산2+1",
-            "Name: 콜라, Price: 1000, Quantity: 10, Promotion: null",
+            "Name: 콜라, Price: 1000, Quantity: 10, Promotion:",
             "Name: 사이다, Price: 1000, Quantity: 8, Promotion: 탄산2+1",
-            "Name: 사이다, Price: 1000, Quantity: 7, Promotion: null",
+            "Name: 사이다, Price: 1000, Quantity: 7, Promotion:",
             "Name: 오렌지주스, Price: 1800, Quantity: 9, Promotion: MD추천상품",
             "Name: 탄산수, Price: 1200, Quantity: 5, Promotion: 탄산2+1",
-            "Name: 물, Price: 500, Quantity: 10, Promotion: null",
+            "Name: 물, Price: 500, Quantity: 10, Promotion:",
             "Name: 비타민워터, Price: 1500, Quantity: 6, Promotion: null",
             "Name: 감자칩, Price: 1500, Quantity: 5, Promotion: 반짝할인",
-            "Name: 감자칩, Price: 1500, Quantity: 5, Promotion: null",
+            "Name: 감자칩, Price: 1500, Quantity: 5, Promotion:",
             "Name: 초코바, Price: 1200, Quantity: 5, Promotion: MD추천상품",
-            "Name: 초코바, Price: 1200, Quantity: 5, Promotion: null",
-            "Name: 에너지바, Price: 2000, Quantity: 5, Promotion: null",
-            "Name: 정식도시락, Price: 6400, Quantity: 8, Promotion: null",
+            "Name: 초코바, Price: 1200, Quantity: 5, Promotion:",
+            "Name: 에너지바, Price: 2000, Quantity: 5, Promotion:",
+            "Name: 정식도시락, Price: 6400, Quantity: 8, Promotion:",
             "Name: 컵라면, Price: 1700, Quantity: 1, Promotion: MD추천상품",
-            "Name: 컵라면, Price: 1700, Quantity: 10, Promotion: null");
+            "Name: 컵라면, Price: 1700, Quantity: 10, Promotion:");
 
 
     private ProductRepository repository;
