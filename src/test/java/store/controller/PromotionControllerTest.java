e9a82c58e9bcf18f647866f2b225ad9b340c3e10
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-09 20:39:48 +0900

    test(PromotionControllerTest) 객체 추가

---
+++ src/test/java/store/controller/PromotionControllerTest.java
@@ -0,0 +1,12 @@
+package store.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class PromotionControllerTest {
+    private PromotionController controller;
+
+    @BeforeEach
+    void set() {
+        this.controller = new PromotionController();
+    }
+}
