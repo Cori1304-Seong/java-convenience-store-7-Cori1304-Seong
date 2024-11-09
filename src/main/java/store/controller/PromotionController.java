46583aa664645e24233f1d579ceb808bd4996cfd
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-09 18:22:53 +0900

    feat: 프로모션 이름 검색과 추가로 줘야할 개수 기능 추가
    - findPromotionByName(): 프로모션 중에서 이름으로 검색
    - calculatePromotionAmount(): 추가로 필요한 프로모션 개수 계산 후 반환

--- src/main/java/store/controller/PromotionController.java
+++ src/main/java/store/controller/PromotionController.java
@@ -13,4 +13,23 @@ public class PromotionController {
         this.promotions = promotionRepository.loadDataFromMarkdown();
     }
 
+    public Promotion findPromotionByName(String name) {
+        for (Promotion promotion : promotions) {
+            if (promotion.getName().equals(name)) {
+                return promotion;
+            }
+        }
+        //TODO 반환 값 다른 것으로 변경 고려
+        return new Promotion("", 0, 0, new Date(), new Date());
+    }
+
+    public int calculatePromotionAmount(int requestAmount, int buyCount, int getCount) {
+        int ZERO = 0;
+        int remainder = requestAmount % (buyCount + getCount);
+
+        if (remainder == buyCount) {
+            return getCount;
+        }
+        return ZERO;
+    }
 }
