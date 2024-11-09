851cbbcdf345d91448fd7f7fc7d75370f7938c49
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-09 21:01:34 +0900

    feat: 구입가능한 목록 출련하는 기능 추가
    - showProducts(): 파라미터로 받은 List<Product> 내용 모두 출력
    - printProductToFormat(): format에 맞춰서 출력

--- src/main/java/store/view/ProductView.java
+++ src/main/java/store/view/ProductView.java
@@ -1,5 +1,22 @@
 package store.view;
 
-public class ProductView {
+import consts.StringConstants;
+import java.util.List;
+import store.model.Product;
 
+public class ProductView {
+    private final String PRODUCT_FORMAT = "- %s %,1d원 %d개%s\n";
+
+    public void showProducts(List<Product> products) {
+        for (Product product : products) {
+            printProductToFormat(product.getName(), product.getPrice(), product.getQuantity(), product.getPromotion());
+        }
+    }
+
+    private void printProductToFormat(String name, int price, int quantity, String promotion) {
+        if (!promotion.isEmpty()) {
+            promotion = StringConstants.SPACE + promotion;
+        }
+        System.out.printf(PRODUCT_FORMAT, name, price, quantity, promotion);
+    }
 }
