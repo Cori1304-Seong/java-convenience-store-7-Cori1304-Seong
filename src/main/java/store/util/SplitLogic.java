2c6a0372010214b01277f5ed62811cb9d12f75f1
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-10 01:07:52 +0900

    feat: 구입 format split 기능 추가
    - splitPurchaseFormat(): "콜라-10" 와 같은 문자열
    ["콜라","10"]으로 반환

--- src/main/java/store/util/SplitLogic.java
+++ src/main/java/store/util/SplitLogic.java
@@ -1,10 +1,16 @@
 package store.util;
 
+import consts.StringConstants;
 
+// TODO 적용할 곳 찾기
 public class SplitLogic {
 
     private SplitLogic() {
 
     }
 
+    static public String[] splitPurchaseFormat(String text) {
+
+        return text.split(StringConstants.HYPHEN);
+    }
 }
