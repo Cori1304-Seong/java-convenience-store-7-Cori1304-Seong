db5a77b846acc1bf9d1eef1d36e316e1896e5c27
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-09 20:45:50 +0900

    test: calculatePromotionAmount() 테스트 함수 추가

--- src/test/java/store/controller/PromotionControllerTest.java
+++ src/test/java/store/controller/PromotionControllerTest.java
@@ -1,6 +1,7 @@
 package store.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +15,15 @@ class PromotionControllerTest {
     Date START_DATE = CustomFormater.convertToDate("2024-01-01");
     Date END_DATE =  CustomFormater.convertToDate("2024-12-31");
 
+    List<int[]> calculateAmountData = List.of(
+            new int[]{3, 2, 1},
+            new int[]{3, 3, 1},
+            new int[]{4, 3, 1},
+            new int[]{5, 2, 1},
+            new int[]{6, 3, 2},
+            new int[]{7, 3, 1}
+    );
+
     @BeforeEach
     void set() {
         this.controller = new PromotionController();
@@ -25,5 +35,18 @@ class PromotionControllerTest {
         Promotion expected = new Promotion(PromotionName, 1, 1, START_DATE, END_DATE);
         Promotion actual = controller.findPromotionByName(PromotionName);
         assertEquals(expected.toString(), actual.toString(), "계산 결과가 다릅니다.");
+    }
+
+    @Test
+    void testCalculatePromotionAmount() {
+        List<Integer> expected = new ArrayList<>(List.of(0, 1, 0, 1, 0, 1));
+        List<Integer> actual = new ArrayList<>();
+        for (int[] numbers : calculateAmountData) {
+            int count = controller.calculatePromotionAmount(numbers[0], numbers[1], numbers[2]);
+            actual.add(count);
+        }
+        System.out.println("정답: " + expected);
+        System.out.println("제출: " + actual);
+        assertEquals(expected, actual, "계산 결과가 다릅니다.");
     }
 }
