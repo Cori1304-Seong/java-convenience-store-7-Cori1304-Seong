a7a4a396e05442442e573e82bfe614ac785e68d6
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-11 01:15:49 +0900

    test: PlannedPurchase 메서드 관련 테스트 추가

--- src/test/java/store/model/PlannedPurchaseTest.java
+++ src/test/java/store/model/PlannedPurchaseTest.java
@@ -3,6 +3,7 @@ package store.model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class PlannedPurchaseTest {
     private PlannedPurchase plannedPurchase;
@@ -10,6 +11,43 @@ class PlannedPurchaseTest {
     @BeforeEach
     public void setUp() {
         plannedPurchase = new PlannedPurchase(new Product("컵라면", 1700, 1, "MD추천상품"), true, 1, 0);
+    }
+
+    @Test
+    void testGetProduct() {
+        Product product = plannedPurchase.getProduct();
+
+
+        assertEquals("컵라면", product.getName());
+        assertEquals(1700, product.getPrice());
+        assertEquals(1, product.getQuantity());
+        assertEquals("MD추천상품", product.getPromotion());
+    }
+
+    @Test
+    void testIsPromotionProduct() {
+        assertTrue(plannedPurchase.getIsPromotionProduct());
+    }
+
+    @Test
+    void testGetCount() {
+
+        assertEquals(1, plannedPurchase.getCount());
+    }
+
+    @Test
+    void testGetGiveawayCount() {
+        assertEquals(0, plannedPurchase.getGiveawayCount());
+    }
+
+    @Test
+    void testProductImmutability() {
+
+        Product retrievedProduct = plannedPurchase.getProduct();
+
+        retrievedProduct.setName("Changed Name");
+
+        assertEquals("컵라면", plannedPurchase.getProduct().getName());
     }
 
 }
