df2e2d8671913ac9b39614916180c300a9db8d04
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-11 01:19:05 +0900

    feat(PlannedPurchaseController) 새로운 객체 추가

---
+++ src/main/java/store/controller/PlannedPurchaseController.java
@@ -0,0 +1,11 @@
+package store.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import store.model.PlannedPurchase;
+
+public class PlannedPurchaseController {
+    private List<PlannedPurchase> plannedPurchases = new ArrayList<>();
+
+
+}
