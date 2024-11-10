f02815369a8499d01247b0d28563d4c96d6c8c6f
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-11 01:06:00 +0900

    feat: PlannedPurchase 필드 get 메서드 추가

--- src/main/java/store/model/PlannedPurchase.java
+++ src/main/java/store/model/PlannedPurchase.java
@@ -13,4 +13,24 @@ public class PlannedPurchase {
         this.count = count;
         this.giveawayCount = giveawayCount;
     }
+
+    public Product getProduct() {
+
+        return new Product(product);
+    }
+
+    public boolean getIsPromotionProduct() {
+
+        return isPromotionProduct;
+    }
+
+    public int getCount() {
+
+        return count;
+    }
+
+    public int getGiveawayCount() {
+
+        return giveawayCount;
+    }
 }
