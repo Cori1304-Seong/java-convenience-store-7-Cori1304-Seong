019fe6ea1835781535c0b7e6700c8a22b7eb9cc6
Cori1304-Seong <andyjm7739@gmail.com>
2024-11-11 19:22:12 +0900

    feat(Order): 객체 추가

---
+++ src/main/java/store/model/Order.java
@@ -0,0 +1,31 @@
+package store.model;
+
+public class Order {
+
+    public final String name;
+    private int count;
+
+    public Order(String name, int count) {
+        this.name = name;
+        this.count = count;
+
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public boolean minusCount(int count) {
+        int result = this.count - count;
+        if (result < 0) {
+            return false;
+        }
+        this.count = result;
+        return true;
+    }
+
+}
